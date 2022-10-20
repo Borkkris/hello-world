388fae4848006671c2a3600201e253f8a4667e55
Borkkris <Borkkris@users.noreply.github.com>
2022-10-20 23:29:05 +0200

    style changes, readme

--- components/Chat.js
+++ components/Chat.js
@@ -3,15 +3,22 @@ import { View, Text, Button} from 'react-native';
 
 
 export default class Chat extends React.Component {
+
+// Passing the name from value={this.state.name} on start.js file
+
   componentDidMount() {
-    let { name } = this.props.route.params;
-    this.props.navigation.setOptions({ title: name });
+    // let { name } = this.props.route.params;
+    // this.props.navigation.setOptions({ title: name });
   }
 
   render() {
 
+    let { color } = this.props.route.params;
+
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={{backgroundColor: color, flex:1, justifyContent: 'center', alignItems: 'center'}}>
+
+        {/* Button to click on to navigate to the Start Screen */}
         <Button
         title='Go to Start'
         onPress={() => this.props.navigation.navigate('Start')}
--- components/Start.js
+++ components/Start.js
@@ -1,30 +1,164 @@
-import React from 'react';
-import { View, Text, Button, TextInput } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+import ScrollViewCommands from 'react-native/Libraries/Components/ScrollView/ScrollViewCommands';
 
-
-export default class Start extends React.Component {
-constructor(props) {
-  super(props);
-  this.state = { name: "" };
-}
+export default class Start extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', color: '' }
+  }
 
   render() {
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>This is the Start Screen</Text>
+      <View style={styles.container}>
+        <ImageBackground source={require('../assets/Background-Image.jpg')}style={styles.image}>
 
-        <TextInput
-         style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-         onChangeText={(name) => this.setState({name})}
-         value={this.state.name}
-         placeholder='Type here ...'
-       />
+            <Text style={styles.title}>.tipIT</Text>
 
-        <Button
-          title='Go to Chat'
-          onPress={() => this.props.navigation.navigate('Chat', { name: this.state.name })} // to navigate to the Chat Screen
-        />
+          <View style={styles.box}>            
+            {/* More than 2 styles we use [] */}
+            <TextInput style={[styles.input, styles.text]}
+              onChangeText={(name) => this.setState({ name })}
+              value={this.state.name}
+              placeholder='Your Name'
+            />
+            <View style={styles.colorWrapper}>
+              <Text style={[styles.textWrapper, styles.label]}>Choose Background Color</Text>
+              <View style={styles.colors}>
+                <TouchableOpacity style={[styles.color, styles.colorBlack]} onPress={() => this.setState({ color: '#090C08' })} />
+                <TouchableOpacity style={[styles.color, styles.colorPurple]} onPress={() => this.setState({ color: '#474056' })} />
+                <TouchableOpacity style={[styles.color, styles.colorGrey]} onPress={() => this.setState({ color: '#8A95A5' })} />
+                <TouchableOpacity style={[styles.color, styles.colorGreen]} onPress={() => this.setState({ color: '#B9C6AE' })} />
+              </View>
+            </View>
+            <View>
+              <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Chat', { name: this.state.name, color: this.state.color })}>
+                <Text style={styles.buttonText}>Start Chatting</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ImageBackground>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  image: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    
+  },
+
+  title: {
+    fontSize: 35,
+    fontWeight: '600',
+    color: '#2b2a2a',
+    letterSpacing: 3,
+    textAlign: 'center',
+    marginTop: 60,
+  },
+
+  box: {
+    backgroundColor: '#ffffffdf',
+    width: '88%',
+    alignItems: 'center',
+    height: '50%',
+    justifyContent: 'space-evenly',
+    borderRadius: 5,
+    padding: 10,
+    marginTop:100,
+  },
+
+  input: {
+    height: 50,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    borderRadius: 5,
+    color: '#686474',
+    fontWeight: '400',
+  },
+
+  text: {
+    color: '#6b6676',
+    fontSize: 16,
+    fontWeight: '700',
+    padding: 5,
+  },
+
+  textWrapper: {
+    color: '#6b6676',
+    fontSize: 16,
+    fontWeight: '700',
+    textAlign: 'center',
+    marginTop: 40,
+  },
+
+  label: {
+    marginBottom: '7%',
+  },
+
+  colorWrapper: {
+    width: '88%',
+    height: '60%',
+    justifyContent: 'center',
+  },
+
+  colors: {
+    flexDirection: 'row',
+    cursor: 'pointer',
+    justifyContent: 'space-between',
+    marginBottom: 40,
+  },
+
+  color: {
+    borderRadius: 20,
+    width: 40,
+    height: 40,
+  },
+
+  colorBlack: {
+    backgroundColor: '#090C08',
+  },
+
+  colorPurple: {
+    backgroundColor: '#474056',
+  },
+
+  colorGrey: {
+    backgroundColor: '#8A95A5',
+  },
+
+  colorGreen: {
+    backgroundColor: '#B9C6AE',
+  },
+
+  buttonWrapper: {
+    width: '88%',
+    flex: 1,
+    justifyContent: 'end',
+  },
+
+  button: {
+    backgroundColor: '#9568445a',
+    height: 50,
+    width: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 25,
+  },
+
+  buttonText: {
+    padding: 10,
+    color: '#2b2a2a',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+})
